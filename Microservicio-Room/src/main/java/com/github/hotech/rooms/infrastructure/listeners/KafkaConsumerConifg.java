47ebbc3881e1601c3c4e1aa22473c3f74e8bdb49
christopherMore1 <christopmore28@gmail.com>
2025-07-02 21:25:16 -0500

    Room image and  docker-compose file

--- Microservicio-Room/src/main/java/com/github/hotech/rooms/infrastructure/listeners/KafkaConsumerConifg.java
+++ Microservicio-Room/src/main/java/com/github/hotech/rooms/infrastructure/listeners/KafkaConsumerConifg.java
@@ -18,7 +18,7 @@ public class KafkaConsumerConifg {
     @Bean
     public ConsumerFactory<String, Object> consumerFactory(){
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "room");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
